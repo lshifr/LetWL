6da2b52c5d009686187d3bccb6d4fc3bcdd423d8
Leonid Shifrin <leonids@wolfram.com>
2020-03-29 18:50:17 +0300

    Add bootstrap installer

---
+++ BootstrapInstall.m
@@ -0,0 +1,34 @@
+BeginPackage["LetWL`Private"]
+
+$projectURL = "https://github.com/lshifr/LetWL/archive/master.zip"
+
+Replace[
+    URLDownload[$projectURL],
+    {
+        $Failed :> $Failed,
+        tempFile_ :> 
+            With[{extracted = ExtractArchive[
+                tempFile, 
+                FileNameJoin[{$UserBaseDirectory, "Applications"}]
+                ]},
+                DeleteFile[tempFile];
+                Replace[
+                    extracted,
+                    {
+                        $Failed :> $Failed,
+                        {dir_, ___} :> With[
+                            {new = FileNameJoin[{DirectoryName @ dir, "LetWL"}]}
+                            ,
+                            If[DirectoryQ[new], 
+                                DeleteDirectory[new, DeleteContents -> True]
+                            ];
+                            RenameDirectory[dir, new];
+                            new
+                        ]
+                    }
+                ]
+            ]
+    }
+]
+
+Block[{$ContextPath}, EndPackage[]]
